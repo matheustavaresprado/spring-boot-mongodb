42334abbdd0236495b77b572d37e5532125eedfa
matheustavaresprado <matheusprado@id.uff.br>
2019-07-07 19:14:47 -0300

    Obtendo um usuário por id

--- src/main/java/com/matheus/springmongo/services/UserService.java
+++ src/main/java/com/matheus/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.matheus.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.springmongo.domain.User;
 import com.matheus.springmongo.repository.UserRepository;
+import com.matheus.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,7 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
-		
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
