e5afd95687a09d6a5f1bc2f8665d76311a0ba0fd
matheustavaresprado <matheusprado@id.uff.br>
2019-07-07 14:47:41 -0300

    Criação do projeto

---
+++ src/main/java/com/matheus/springmongo/Springmongo2Application.java
@@ -0,0 +1,13 @@
+package com.matheus.springmongo; 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmongo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmongo2Application.class, args);
+	}
+
+}
