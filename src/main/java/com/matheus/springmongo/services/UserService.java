65d69dd611f858d45da648f755d9c7ba6607b3d9
matheustavaresprado <matheusprado@id.uff.br>
2019-07-07 19:41:42 -0300

    Adicioando usuário com POST

--- src/main/java/com/matheus/springmongo/services/UserService.java
+++ src/main/java/com/matheus/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheus.springmongo.domain.User;
+import com.matheus.springmongo.dto.UserDTO;
 import com.matheus.springmongo.repository.UserRepository;
 import com.matheus.springmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
