61952c1b872452b3b4c73aa9300b201f3172a42c
matheustavaresprado <matheusprado@id.uff.br>
2019-07-14 13:14:53 -0300

    Obtendo um post por id

---
+++ src/main/java/com/matheus/springmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.matheus.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheus.springmongo.domain.Post;
+import com.matheus.springmongo.repository.PostRepository;
+import com.matheus.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
