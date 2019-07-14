6a2ce79d90742f2a3eca8f70a691b21b92be6ff6
matheustavaresprado <matheusprado@id.uff.br>
2019-07-14 15:23:37 -0300

    Consulta com @Query

--- src/main/java/com/matheus/springmongo/repository/PostRepository.java
+++ src/main/java/com/matheus/springmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.matheus.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.matheus.springmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.matheus.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
